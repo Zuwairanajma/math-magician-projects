a296fc369b788af5653e216ae54d8de2da0dab9e
Zuwairanajma <juwairiyyasadiq@gmail.com>
2023-08-16 18:16:25 +0100

    Add events requirements

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,40 +1,49 @@
-export default function Calculator() {
+import { React, useState } from 'react';
+import PropTypes from 'prop-types';
+import calculate from '../logic/calculate';
+import BoardFeature from './BoardFeature';
+
+function Interface({ showValue }) {
   return (
-    <div className="Outer-Container">
-      <div className="Inner-Container">
-        <p>0</p>
-      </div>
-      <BoardFeature />
+    <div className="Inner-Container">
+      <p>{showValue}</p>
     </div>
   );
 }
 
-function BoardFeature() {
+Interface.propTypes = {
+  showValue: PropTypes.string,
+};
+
+Interface.defaultProps = {
+  showValue: '0',
+};
+
+export default function Calculator() {
+  const [showValue, setShowValue] = useState('0');
+  const [infoCalculate, setInfoCalculate] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const hanndleBoardClick = (buttonName) => {
+    const newInfo = calculate(infoCalculate, buttonName);
+    setInfoCalculate(newInfo);
+
+    if (newInfo.next !== null) {
+      setShowValue(newInfo.next);
+    } else if (newInfo.total !== null) {
+      setShowValue(newInfo.total);
+    } else {
+      setShowValue('0');
+    }
+  };
+
   return (
-    <div className="button">
-      <button type="submit" className="btn" name="AC">AC</button>
-      <button type="submit" className="btn" name="+/-">+/-</button>
-      <button type="submit" className="btn" name="%">%</button>
-      <button type="submit" className="btn btn-orange" name="÷">÷</button>
-      <br />
-      <button type="submit" className="btn" name="7">7</button>
-      <button type="submit" className="btn" name="8">8</button>
-      <button type="submit" className="btn" name="9">9</button>
-      <button type="submit" className="btn btn-orange" name="x">x</button>
-      <br />
-      <button type="submit" className="btn" name="4">4</button>
-      <button type="submit" className="btn" name="5">5</button>
-      <button type="submit" className="btn" name="6">6</button>
-      <button type="submit" className="btn btn-orange" name="-">-</button>
-      <br />
-      <button type="submit" className="btn" name="4">1</button>
-      <button type="submit" className="btn" name="5">2</button>
-      <button type="submit" className="btn" name="6">3</button>
-      <button type="submit" className="btn btn-orange" name="+">+</button>
-      <br />
-      <button type="submit" className="zero" name="0">0</button>
-      <button type="submit" className="btn" name=".">.</button>
-      <button type="submit" className="btn btn-orange" name="=">=</button>
+    <div className="Outer-Container">
+      <BoardFeature onClickButton={hanndleBoardClick} />
+      <Interface showValue={showValue} />
     </div>
   );
 }
